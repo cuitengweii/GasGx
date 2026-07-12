c51161e9eb55c416e134f3bc86172727dba4e0f4
Allen <cuitengweii@gmail.com>
2026-07-12 11:39:55 +0800

    fix(quote): 主操作按钮右对齐

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public44';
+    const version = '20260329public45';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
