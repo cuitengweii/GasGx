24394acc28c0e3785c1debb7a1a477774293b7e2
Allen <cuitengweii@gmail.com>
2026-07-13 07:45:31 +0800

    style(quote): 强化折算价格标题样式

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public85';
+    const version = '20260329public86';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
