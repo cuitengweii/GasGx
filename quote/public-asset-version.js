1b51e224f8cc3847124877deccdca780a6e69401
Allen <cuitengweii@gmail.com>
2026-07-12 19:55:12 +0800

    fix(quote): 更新生成报价单按钮文案

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public65';
+    const version = '20260329public66';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
