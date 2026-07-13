da6d095f682b62d97f2bc1128abe2ae87e57877a
Allen <cuitengweii@gmail.com>
2026-07-13 09:08:10 +0800

    style(quote): 调整分享页手机版标题顺序

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public98';
+    const version = '20260329public99';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
