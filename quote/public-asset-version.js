ba58ad5a2638499d1feee8959f7451bc3dfa2770
Allen <cuitengweii@gmail.com>
2026-07-13 08:31:33 +0800

    fix(quote): 适配手机版报价表布局

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public92';
+    const version = '20260329public93';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
