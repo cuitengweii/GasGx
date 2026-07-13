87795cbfeb97fac3790d9f2b632c3d5a98bb25a3
Allen <cuitengweii@gmail.com>
2026-07-13 08:24:27 +0800

    style(quote): 增加主配置绿色边条

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public90';
+    const version = '20260329public91';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
