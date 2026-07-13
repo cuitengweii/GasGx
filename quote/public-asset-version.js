e75ba05c5db21e9025418f8f9d078a2795569306
Allen <cuitengweii@gmail.com>
2026-07-13 08:56:43 +0800

    style(quote): 将选中对号改为白色

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260329public95';
+    const version = '20260329public97';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
