0eef54dc3256cd044f84c55f9fa6ca6e6be5c0cc
Allen <cuitengweii@gmail.com>
2026-07-24 06:23:12 +0800

    style(quote): 统一预览页深色主题

--- quote/public-asset-version.js
+++ quote/public-asset-version.js
@@ -1,5 +1,5 @@
 (function initQuotePublicAssetVersion() {
-    const version = '20260724quote-shell23';
+    const version = '20260724quote-shell24';
     window.AMS_QUOTE_PUBLIC_ASSET_VERSION = version;
     window.AMSLoadQuotePublicStyles = function loadQuotePublicStyles(href) {
         if (!href) return;
